acb53b1d8f4e0d6db702a56cc072fbf15bca2149
Sahir Mody <sahir.mody@gmail.com>
2020-10-26 23:46:54 -0400

    Add exec linkedin links

--- styles/membership.js
+++ styles/membership.js
@@ -1,19 +1,16 @@
 var brother_pic_src = ""
 const LINKEDIN_LOGO_URL = 'https://cdn.iconscout.com/icon/free/png-256/linkedin-36-151077.png'
 
-function hover(element) {
+function hoverBrotherPic(element) {
   brother_pic_src = element.getAttribute('src');
   element.setAttribute('src', LINKEDIN_LOGO_URL);
 }
 
-function unhover(element) {
+function unhoverBrotherPic(element) {
   element.setAttribute('src', brother_pic_src);
 }
 
-
-/*
-
-ADD ONCLICK FUNCTION TO LINK TO LINKEDIN
-HAVE THE ONCLICK RESET THE BROTHER PIC BEFORE LINKING
-
-*/
+function redirectBrotherPic(element, link) {
+  element.setAttribute('src', brother_pic_src);
+  window.location.href = link;
+}
