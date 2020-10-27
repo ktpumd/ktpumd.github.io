d0e24144a691b45a1e15236cc9887a55b314e75e
Sahir Mody <sahir.mody@gmail.com>
2020-10-26 22:04:30 -0400

    Add linkedin logos

--- styles/membership.js
+++ styles/membership.js
@@ -1,1 +1,19 @@
+var brother_pic_src = ""
+const LINKEDIN_LOGO_URL = 'https://cdn.iconscout.com/icon/free/png-256/linkedin-36-151077.png'
 
+function hover(element) {
+  brother_pic_src = element.getAttribute('src');
+  element.setAttribute('src', LINKEDIN_LOGO_URL);
+}
+
+function unhover(element) {
+  element.setAttribute('src', brother_pic_src);
+}
+
+
+/*
+
+ADD ONCLICK FUNCTION TO LINK TO LINKEDIN
+HAVE THE ONCLICK RESET THE BROTHER PIC BEFORE LINKING
+
+*/
