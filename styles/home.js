6853a7106d186dcea430ea6bf249fee96c0bbfbf
Sahir Mody <sahir.mody@gmail.com>
2021-01-02 22:56:03 -0500

    Add JS to generate company logo boxes

--- styles/home.js
+++ styles/home.js
@@ -1,1 +1,31 @@
 
+COMPANY_IMAGES = [
+    "google.png",
+    "amazon.png",
+    "facebook.png",
+    "microsoft.png",
+    "capitalone.png",
+    "blackstone.png",
+    "nasa.gif",
+    "adobe.png",
+    "ibm.jpg",
+    "chewy.png",
+    "twosigma.png",
+    "EY.png"
+]
+
+function generateCompanyBoxes() {
+
+  var companySection = $( "#COMPANIES" );
+
+  COMPANY_IMAGES.forEach((item, i) => {
+
+    companySection.append('<div class="col-lg-3 col-md-4 col-6 d-block mb-4 h-100" id="company' + i.toString() + '"></div>');
+
+    var companyImage = $( "#company" +  i.toString());
+    companyImage.append('<img class="img-fluid img-thumbnail" src="assets/alumni-connections/' + item + '">');
+
+  });
+
+
+}
