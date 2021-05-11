e9a4632be83e7b098d75536367d48109a34c6958
Sahir Mody <sahir.mody@gmail.com>
2021-05-11 00:08:17 -0400

    Update for Grad Spring 2021

--- assets/js/membership.js
+++ assets/js/membership.js
@@ -43,15 +43,15 @@ function redirectBrotherPic(element, link) {
 
 EXEC = {
 
-  'President': ['Dominique', 'Rafael', 'https://www.linkedin.com/in/dominique-r-a853ab123/'],
-  'Vice President': ['Sahir', 'Mody', 'https://www.linkedin.com/in/sahir-mody/'],
-  'Director of Membership': ['Samantha', 'Tang', 'https://www.linkedin.com/in/stang10/'],
-  'Director of Standards': ['Jenny', 'Corzo', 'https://www.linkedin.com/in/jencorzo/'],
-  'Director of Professional Development': ['Shlok', 'Khandelwal', 'https://www.linkedin.com/in/shlokkh/'],
-  'Director of Finance': ['Rida', 'Habib', 'https://www.linkedin.com/in/rida-habib-29065316b/'],
-  'Director of Philanthropy': ['Ummey', 'Hossain', 'https://www.linkedin.com/in/ummey-hossain/'],
-  'Director of Technology': ['Esther', 'Zhang', 'https://www.linkedin.com/in/esther-zhang-036237184/'],
-  'Director of Engagement': ['Devika', 'Govindarajan']
+  'President': ['Samantha', 'Tang', 'https://www.linkedin.com/in/stang10/'],
+  'Vice President': ['Devika', 'Govindarajan'],
+  'Director of Membership': ['Vid', 'Vashist', 'https://www.linkedin.com/in/vidushivashist/'],
+  'Director of Standards': ['Katie', 'Kemp', 'https://www.linkedin.com/in/katherineekemp/'],
+  'Director of Professional Development': ['Sahir', 'Mody', 'https://www.linkedin.com/in/sahir-mody/'],
+  'Director of Finance': ['Edmund', 'Li', 'https://www.linkedin.com/in/liedmund/'],
+  'Director of Philanthropy': ['Matthew', 'Gouzoulis', 'https://www.linkedin.com/in/mgouz/'],
+  'Director of Technology': ['Austin', 'Hwa', 'https://www.linkedin.com/in/austin-hwa-574b10156/'],
+  'Director of Engagement': ['Janaki', 'Patel', 'https://www.linkedin.com/in/janaki-patel-147437198/']
 
 }
 
@@ -128,66 +128,59 @@ BROTHERS = [
 
   ['Akriti', 'Tripathi', 'https://www.linkedin.com/in/akriti-tripathi/'],
   ['Amanda', 'Ma', 'https://www.linkedin.com/in/amandama2300/'],
-  ['Amy', 'Zheng', 'https://www.linkedin.com/in/amy--zheng/'],
-  ['Andrew', 'Cunniff', 'https://www.linkedin.com/in/andrew-cunniff/'],
+  ['Ananya', 'Nadig', 'https://www.linkedin.com/in/ananyanadig/'],
   ['Andrew', 'Stehman', 'https://www.linkedin.com/in/andrewstehman/'],
-  ['Austin', 'Hwa', 'https://www.linkedin.com/in/austin-hwa-574b10156/'],
-
+  ['Casey', 'Puentes', 'https://www.linkedin.com/in/caseypuentes/'],
   ['Christian', 'Tonti', 'https://www.linkedin.com/in/christian-tonti/'],
-  ['Christopher', 'Tang', 'https://www.linkedin.com/in/christopher-tang-25aa101b8'],
-  ['Daniel', 'Holten', 'https://www.linkedin.com/in/daniel-holten-5a9250162/'],
-  ['Diana', 'Kerr', 'https://www.linkedin.com/in/diana-kerr-2249a1193/'],
-  ['Edmund', 'Li', 'https://www.linkedin.com/in/liedmund/'],
-  ['Emily', 'Chang', 'https://www.linkedin.com/in/e-chang/'],
 
+  ['Christopher', 'Tang', 'https://www.linkedin.com/in/christopher-tang-25aa101b8'],
+  ['Diana', 'Kerr', 'https://www.linkedin.com/in/diana-kerr-2249a1193/'],
+  ['Emily', 'Chang', 'https://www.linkedin.com/in/e-chang/'],
   ['Eric', 'Gorner', 'https://www.linkedin.com/in/eric-gorner-53851a17a'],
+  ['Esther', 'Zhang', 'https://www.linkedin.com/in/esther-zhang-036237184/'],
   ['Ethan', 'Smiley', 'https://www.linkedin.com/in/ethan-smiley-123463181/'],
+
+  ['Grant', 'Tamrakar', 'https://www.linkedin.com/in/grant-tamrakar-802b90181/'],
   ['Harleen', 'Kaur', 'https://www.linkedin.com/in/kaur-h/'],
   ['Hira', 'Mahmood', 'https://www.linkedin.com/in/h-mahmood/'],
-  ['Janaki', 'Patel', 'https://www.linkedin.com/in/janaki-patel-147437198/'],
   ['Jasmine', 'Okebugwu', 'https://www.linkedin.com/in/jasmineokebugwu/'],
-
-  ['Jessica', 'Kerry', 'https://www.linkedin.com/in/jdkerry/'],
-  ['John', 'Gao', 'https://www.linkedin.com/in/jgao1238/'],
   ['Joshua', 'Choi', 'https://www.linkedin.com/in/joshua--choi/'],
   ['June', 'Tian', 'https://www.linkedin.com/in/chengzi-tian-ab2923198/'],
-  ['Kate', 'Vo', 'https://www.linkedin.com/in/katherine-vo/'],
-  ['Kati', 'Ilieva', 'https://www.linkedin.com/in/katrin-sofi-ilieva/'],
 
-  ['Katie', 'Kemp', 'https://www.linkedin.com/in/katherineekemp/'],
+  ['Kate', 'Vo', 'https://www.linkedin.com/in/katherine-vo/'],
   ['Katie', 'Vincler', 'https://www.linkedin.com/in/katherine-vincler/'],
+  ['Kevin', 'Hall', 'https://www.linkedin.com/in/khall04/'],
   ['Lauren', 'LaTeef', 'https://www.linkedin.com/in/lauren-lateef-325446192/'],
   ['Leo', 'Wu', 'https://www.linkedin.com/in/leowu39'],
   ['Lillian', 'Nose', 'https://www.linkedin.com/in/lillian-nose/'],
+
+  ['Lily', 'Etareri', 'https://www.linkedin.com/in/eloho-etareri'],
   ['Maggie', 'Zhou', 'https://www.linkedin.com/in/maggie-zhou-991b93194/'],
-
-  ['Matthew', 'Gouzoulis', 'https://www.linkedin.com/in/mgouz/'],
-  ['Matthew', 'Leins', 'https://www.linkedin.com/in/matthew-leins/'],
+  ['Mahum', 'Qadeer', 'https://www.linkedin.com/in/mahumqadeer'],
   ['Mei', 'Blatchford', 'https://www.linkedin.com/in/mei-an-blatchford-39b839187/'],
-  ['Melvyn', 'Hullana', 'https://www.linkedin.com/in/melvynh/'],
   ['Nik', 'Nair', 'https://www.linkedin.com/in/nik-nair/'],
-  ['Nithin', 'Richard', 'https://www.linkedin.com/in/nithin-richard/'],
-
-  ['Prachi', 'Gaur', 'https://www.linkedin.com/in/pgaur1/'],
-  ['Priyanka', 'Kishore', 'https://www.linkedin.com/in/priyanka-m-kishore/'],
   ['Rafay', 'Khan', 'https://www.linkedin.com/in/muhammadrafaykhan/'],
-  ['Rahul', 'Krishnan', 'https://www.linkedin.com/in/rahul-krishnan-/'],
-  ['Richard', 'Bui', 'https://www.linkedin.com/in/richard-bui-6ba38719a/'],
-  ['Rishi', 'Papani', 'https://www.linkedin.com/in/rishi-s-papani-736149190/'],
 
+  ['Richard', 'Bui', 'https://www.linkedin.com/in/richard-bui-6ba38719a/'],
+  ['Rida', 'Habib', 'https://www.linkedin.com/in/rida-habib-29065316b/'],
+  ['Rishi', 'Papani', 'https://www.linkedin.com/in/rishi-s-papani-736149190/'],
   ['Rishi', 'Parikh', 'https://www.linkedin.com/in/rrparikh/'],
-  ['Ryan', 'Havel', 'https://www.linkedin.com/in/havelr/'],
-  ['Ryan', 'Lam', 'https://www.linkedin.com/in/ryan-lam-151b00168/'],
   ['Shreyas', 'Vaderiyattil', 'https://www.linkedin.com/in/shreyasvaderiyattil'],
-  ['Siraj', 'Ayornu', 'https://www.linkedin.com/in/sayornu/'],
   ['Sophie', 'Jorgensen', 'https://www.linkedin.com/in/sophie-jorgensen/'],
 
+  ['Steven', 'Garrido', 'https://www.linkedin.com/in/stevenagarrido/'],
+  ['Suraj', 'Narang', 'https://www.linkedin.com/in/suraj-narang'],
   ['Thy', 'Hoang', 'https://www.linkedin.com/in/thy-hoang-8730b81a4/'],
-  ['Timmy', 'Trapaidze', 'https://www.linkedin.com/in/timmy-trapaidze/'],
-  ['Val', 'Arriola-Pomasoncco', 'https://www.linkedin.com/in/valeriarriola/'],
-  ['Vid', 'Vashist', 'https://www.linkedin.com/in/vidushivashist/'],
+  ['Tina', 'Le', 'https://www.linkedin.com/in/tina-le218/'],
+  ['Vidit', 'Makwana', 'https://bit.ly/viditmakwana'],
   ['Vikram', 'Adipudi', 'https://www.linkedin.com/in/vikram-adipudi-27025519b/'],
-  ['Yomi', 'Omogbehin', 'https://www.linkedin.com/in/yomi-omo/']
+
+  ['Vivek', 'Waghray', 'https://www.linkedin.com/in/vivek-waghray-812079208/'],
+  ['',''],
+  ['',''],
+  ['',''],
+  ['',''],
+  ['','']
 
 ]
 
@@ -262,13 +255,16 @@ ALUMNI = {
   "Alpha": [
     ['Ava', 'Williams', 'https://www.linkedin.com/in/ava--williams/'],
     ['Daniel', 'Aivazov', 'https://www.linkedin.com/in/daniel-aivazov-ab6933139/'],
+    ['Daniel', 'Holten', 'https://www.linkedin.com/in/daniel-holten-5a9250162/'],
     ['Ekansh', 'Vinaik', 'https://www.linkedin.com/in/evinaik/'],
     ['Harry', 'Irvine', 'https://www.linkedin.com/in/harry-irvine-iv/'],
+    ['Jessica', 'Kerry', 'https://www.linkedin.com/in/jdkerry/'],
     ['JT', 'Field', 'https://www.linkedin.com/in/jtfield/'],
     ['Marc', 'Marcelin', 'https://www.linkedin.com/in/marc-marcelin-6b4aab13b/'],
     ['Matthew', 'Aird', 'https://www.linkedin.com/in/matthew-aird/'],
     ['Niharika', 'Sahay', 'https://www.linkedin.com/in/niharika-sahay/'],
     ['Rachit', 'Chadha', 'https://www.linkedin.com/in/rachitchadha/'],
+    ['Ryan', 'Lam', 'https://www.linkedin.com/in/ryan-lam-151b00168/'],
     ['Swati', 'Adipudi', 'https://www.linkedin.com/in/swati-adipudi'],
     ['Thaddeus', 'Lee-Tyson', 'https://www.linkedin.com/in/thaddeusleetyson/']
   ],
@@ -282,17 +278,43 @@ ALUMNI = {
     ['Jeffrey', 'Najmi', 'https://www.linkedin.com/in/jeffrey-najmi-192a77162/'],
     ['Michael', 'Stripling', 'https://www.linkedin.com/in/michael-stripling-898053138/'],
     ['Mike', 'Walker', 'https://www.linkedin.com/in/mwalker1380/'],
+    ['Prachi', 'Gaur', 'https://www.linkedin.com/in/pgaur1/'],
+    ['Rahul', 'Krishnan', 'https://www.linkedin.com/in/rahul-krishnan-/'],
     ['Ronit', 'Gopalani', 'https://www.linkedin.com/in/ronit-gopalani-7a816a62/'],
     ['Ryan', 'McCarthy', 'https://www.linkedin.com/in/ryan-mccarthy-a664b416b/'],
+    ['Ryan', 'Havel', 'https://www.linkedin.com/in/havelr/'],
     ['Shad', 'Khan', 'https://www.linkedin.com/in/shadikhan/'],
+    ['Shlok', 'Khandelwal', 'https://www.linkedin.com/in/shlokkh/'],
     ['Tyler', 'Behr', 'https://www.linkedin.com/in/tylerbehr/']
   ],
 
   "Gamma": [
     ['Deep', 'Patel', 'https://www.linkedin.com/in/dpatl/'],
+    ['Dominique', 'Rafael', 'https://www.linkedin.com/in/dominique-r-a853ab123/'],
     ['Ines', 'Benmohamed', 'https://www.linkedin.com/in/ines-benmohamed-7a1717181/'],
     ['Isa', 'Ahmed', 'https://www.linkedin.com/in/isaahmad/'],
-    ['Kendall', 'Wong', 'https://www.linkedin.com/in/kendall-wong/']
+    ['Jenny', 'Corzo', 'https://www.linkedin.com/in/jencorzo/'],
+    ['Kendall', 'Wong', 'https://www.linkedin.com/in/kendall-wong/'],
+    ['Siraj', 'Ayornu', 'https://www.linkedin.com/in/sayornu/'],
+    ['Timmy', 'Trapaidze', 'https://www.linkedin.com/in/timmy-trapaidze/']
+  ],
+
+  "Delta": [
+    ['Amy', 'Zheng', 'https://www.linkedin.com/in/amy--zheng/'],
+    ['Andrew', 'Cunniff', 'https://www.linkedin.com/in/andrew-cunniff/'],
+    ['Kati', 'Ilieva', 'https://www.linkedin.com/in/katrin-sofi-ilieva/'],
+    ['Ummey', 'Hossain', 'https://www.linkedin.com/in/ummey-hossain/'],
+    ['Val', 'Arriola-Pomasoncco', 'https://www.linkedin.com/in/valeriarriola/']
+  ],
+
+  "Epsilon": [
+    ['John', 'Gao', 'https://www.linkedin.com/in/jgao1238/'],
+    ['Matthew', 'Leins', 'https://www.linkedin.com/in/matthew-leins/'],
+    ['Melvyn', 'Hullana', 'https://www.linkedin.com/in/melvynh/'],
+    ['Nithin', 'Richard', 'https://www.linkedin.com/in/nithin-richard/'],
+    ['Priyanka', 'Kishore', 'https://www.linkedin.com/in/priyanka-m-kishore/'],
+    ['Yomi', 'Omogbehin', 'https://www.linkedin.com/in/yomi-omo/']
+
   ]
 
 }
@@ -320,5 +342,3 @@ function addAlumni() {
     alumniSection.appendChild(newRow);
   }
 }
-
-
