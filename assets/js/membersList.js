812f23a4fb430ba5acf7f1190306c87709fb5353
jsshapiro02 <84941948+jsshapiro02@users.noreply.github.com>
2023-08-29 01:28:59 -0400

    [Fall23] updated brother images, alumni list, new exec

--- assets/js/membersList.js
+++ assets/js/membersList.js
@@ -1,238 +1,261 @@
 var membersList = {
 
 
-/*
- ******************************************************************
- *
- *
- * EXEC BOARD
- *
- *
- ******************************************************************
- */
-
-
-"ExecBoard":
-{
-
-  "President":
+  /*
+   ******************************************************************
+   *
+   *
+   * EXEC BOARD
+   *
+   *
+   ******************************************************************
+   */
+  
+  
+  "ExecBoard":
+  {
+  
+    "President":
+      ["Antonio", "Farias"],
+    "Vice President":
+      ["Vivek", "Waghray"],
+    "Director of Membership":
+      ["Angad", "Singh"],
+    "Director of Standards":
+      ["Jacob", "Shapiro"],
+    "Director of Professional Development":
+      ["Eric", "Gorner"],
+    "Director of Finance":
+      ["Arihant", "Gupta"],
+    "Director of Philanthropy":
+      ["Maya", "Chelar"],
+    "Director of Technology":
+      ["Sana", "Choudhry"],
+    "Director of Engagement":
+      ["Nikita", "Khapre"]
+  
+  },
+  
+  
+  /*
+  ******************************************************************
+  *
+  *
+  * BROTHERS
+  *
+  *
+  ******************************************************************
+  */
+  
+  
+  "Brothers":
+  [
+  
+  
+    ["Akshay", "Subramaniam"],
+    ["Ananya", "Nadig"],
+    ["Andrew", "Le"],
+    ["Cassiel", "Arcilla"],
+    ["Christopher", "Tang"],
+    ["Diana", "Huynh"],
+    ["Eddie", "Zhang"],
+    ["Garrett", "Amos"],
+    ["Isabelle", "Summann"],
+    ["Janet", "Chan"],
+    ["Julius", "Angelo"],
+    ["Lily", "Etareri"],
+    ["Mahum", "Qadeer"],
+    ["Malaya", "Heflin"],
+    ["Mickey", "Santomartino"],
+    ["Noor", "Qureshi"],
+    ["Poojan", "Patel"],
+    ["Rishi", "Papani"],
+    ["Rishiraj", "Ghosh"],
+    ["Riya", "Boyapati"],
+    ["Ron", "Reyes"],
+    ["Sai", "Posa"],
+    ["Samantha", "Tang"],
+    ["Sierra", "Hopkins"],
+    ["Soojee", "Fee"],
+    ["Sophie", "Jorgensen"],
+    ["Sriya", "Sogal"],
+    ["Suraj", "Narang"],
+    ["Taylor", "Wong"],
+    ["Thy", "Hoang"],
+    ["Tia", "Tran"],
+    ["Tina", "Le"],
+    ["Tina", "Ngo"],
+    ["Zoe", "Cheng"],
+    ["Valerie", "Yen"],
+    ["Erin", "Maynard"],
+    ["Simmi", "Nirula"],
+    ["Victor", "Su"],
+    ["Sophia", "Gonzales"],
+    ["Gandhar", "Viragi"],
+    ["Zane", "Rawlings"],
+    ["Vikram", "Jasti"],
+    ["Eshita", "Mehrotra"],
+    ["Saatvik", "Gangavarapu"],
+    ["Casey", "Majewski"],
+    ["Taegon", "Hibbitts"],
+    ["Kadin", "Fisher"],
+    ["Jaskaran", "Gill"],
+    ["Patrick", "Healy"],
+    ["Abhijay", "Shukla"],
+    ["Chetanya", "Makkar"],
+    ["Michelle", "Nguyen"],
+    ["Grace","Gellerman"]
+  
+  ],
+  /*
+  ******************************************************************
+  *
+  *
+  * ALUMNI
+  *
+  *
+  ******************************************************************
+  */
+  
+  "Alumni" : {
+  
+  "Founders": [
+    ["Alicia", "Curcuru"],
+    ["Ben", "Nwokeleme"],
+    ["Chantelle", "Rodriguez"],
+    ["Garima", "Dewan"],
+    ["Jonathan", "Lee"],
+    ["Jordyn", "Berry"],
+    ["Lydia", "Spurrier"],
+    ["Sukhneet", "Kaur"]
+  ],
+  
+  "Alpha": [
+    ["Ava", "Williams"],
+    ["Chris", "Truong"],
+    ["Daniel", "Aivazov"],
+    ["Daniel", "Holten"],
+    ["Ekansh", "Vinaik"],
+    ["Harry", "Irvine"],
+    ["Jessica", "Kerry"],
+    ["JT", "Field"],
+    ["Marc", "Marcelin"],
+    ["Matthew", "Aird"],
+    ["Niharika", "Sahay"],
+    ["Rachit", "Chadha"],
+    ["Ryan", "Lam"],
+    ["Swati", "Adipudi"],
+    ["Thaddeus", "Lee-Tyson"]
+  ],
+  
+  "Beta": [
+    ["Ameya", "Sampath"],
+    ["Christian", "Tonti"],
+    ["Corie", "Brown"],
+    ["Faatemah", "Mallick"],
+    ["Isabelle", "Stevens"],
+    ["James", "Bates"],
+    ["Jeffrey", "Najmi"],
+    ["Michael", "Stripling"],
+    ["Mike", "Walker"],
+    ["Prachi", "Gaur"],
+    ["Rahul", "Krishnan"],
+    ["Ronit", "Gopalani"],
+    ["Ryan", "McCarthy"],
+    ["Ryan", "Havel"],
+    ["Shad", "Khan"],
+    ["Shlok", "Khandelwal"],
+    ["Tyler", "Behr"]
+  ],
+  
+  "Gamma": [
+    ["Amanda", "Ma"],
+    ["Austin", "Hwa"],
+    ["Deep", "Patel"],
+    ["Dominique", "Rafael"],
+    ["Esther", "Zhang"],
+    ["Ethan", "Smiley"],
+    ["Ines", "Benmohamed"],
+    ["Isa", "Ahmed"],
+    ["Jenny", "Corzo"],
+    ["Katie", "Kemp"],
+    ["Kendall", "Wong"],
+    ["Nik", "Nair"],
+    ["Rafay", "Khan"],
+    ["Rida", "Habib"],
+    ["Sahir", "Mody"],
+    ["Siraj", "Ayornu"],
+    ["Timmy", "Trapaidze"]
+  ],
+  
+  "Delta": [
+    ["Amy", "Zheng"],
+    ["Andrew", "Cunniff"],
+    ["Hira", "Mahmood"],
+    ["Jasmine", "Okebugwu"],
+    ["Kati", "Ilieva"],
+    ["Katie", "Vincler"],
+    ["Lillian", "Nose"],
+    ["Matthew", "Gouzoulis"],
+    ["Ummey", "Hossain"],
+    ["Val", "Arriola-Pomasoncco"],
+    ["Edmund", "Li"],
+    ["Emily", "Chang"],
     ["Devika", "Govindarajan"],
-  "Vice President":
-    ["Vid", "Vashist"],
-  "Director of Membership":
-    ["Harleen", "Kaur"],
-  "Director of Standards":
-    ["Angad", "Singh"],
-  "Director of Professional Development":
-    ["Vidit", "Makwana"],
-  "Director of Finance":
-    ["Grace", "Gellerman"],
-  "Director of Philanthropy":
-    ["Vivek", "Waghray"],
-  "Director of Technology":
-    ["Janaki", "Patel"],
-  "Director of Engagement":
-    ["Antonio", "Farias"]
-
-},
-
-
-/*
-******************************************************************
-*
-*
-* BROTHERS
-*
-*
-******************************************************************
-*/
-
-
-"Brothers":
-[
-
-
-  ["Abigail", "Miller"],
-  ["Akshay", "Subramaniam"],
-  ["Ananya", "Nadig"],
-  ["Andrew", "Le"],
-  ["Arihant", "Gupta"],
-  ["Casey", "Puentes"],
-  ["Cassiel", "Arcilla"],
-  ["Christopher", "Tang"],
-  ["Diana", "Huynh"],
-  ["Eddie", "Zhang"],
-  ["Edmund", "Li"],
-  ["Emily", "Chang"],
-  ["Eric", "Gorner"],
-  ["Garrett", "Amos"],
-  ["Hrishi", "Chintagumpala"],
-  ["Isabelle", "Summann"],
-  ["Jacob", "Shapiro"],
-  ["Janet", "Chan"],
-  ["Joaquin", "Panambo"],
-  ["Julius", "Angelo"],
-  ["Lily", "Etareri"],
-  ["Lily", "Le"],
-  ["Mahum", "Qadeer"],
-  ["Malaya", "Heflin"],
-  ["Maya", "Chelar"],
-  ["Mickey", "Santomartino"],
-  ["Nikita", "Khapre"],
-  ["Noor", "Qureshi"],
-  ["Patrick", "Hogan"],
-  ["Poojan", "Patel"],
-  ["Rishi", "Papani"],
-  ["Rishi", "Parikh"],
-  ["Rishiraj", "Ghosh"],
-  ["Riya", "Boyapati"],
-  ["Ron", "Reyes"],
-  ["Sai", "Posa"],
-  ["Samantha", "Tang"],
-  ["Sana", "Choudhry"],
-  ["Sierra", "Hopkins"],
-  ["Soojee", "Fee"],
-  ["Sophie", "Jorgensen"],
-  ["Sriya", "Sogal"],
-  ["Suraj", "Narang"],
-  ["Taylor", "Wong"],
-  ["Thy", "Hoang"],
-  ["Tia", "Tran"],
-  ["Tina", "Le"],
-  ["Tina", "Ngo"],
-  ["Zoe", "Cheng"],
-  ["Zoey", "Anastasiadis"]
-
-
-],
-
-
-/*
-******************************************************************
-*
-*
-* ALUMNI
-*
-*
-******************************************************************
-*/
-
-"Alumni" : {
-
-"Founders": [
-  ["Alicia", "Curcuru"],
-  ["Ben", "Nwokeleme"],
-  ["Chantelle", "Rodriguez"],
-  ["Garima", "Dewan"],
-  ["Jonathan", "Lee"],
-  ["Jordyn", "Berry"],
-  ["Lydia", "Spurrier"],
-  ["Sukhneet", "Kaur"]
-],
-
-"Alpha": [
-  ["Ava", "Williams"],
-  ["Chris", "Truong"],
-  ["Daniel", "Aivazov"],
-  ["Daniel", "Holten"],
-  ["Ekansh", "Vinaik"],
-  ["Harry", "Irvine"],
-  ["Jessica", "Kerry"],
-  ["JT", "Field"],
-  ["Marc", "Marcelin"],
-  ["Matthew", "Aird"],
-  ["Niharika", "Sahay"],
-  ["Rachit", "Chadha"],
-  ["Ryan", "Lam"],
-  ["Swati", "Adipudi"],
-  ["Thaddeus", "Lee-Tyson"]
-],
-
-"Beta": [
-  ["Ameya", "Sampath"],
-  ["Christian", "Tonti"],
-  ["Corie", "Brown"],
-  ["Faatemah", "Mallick"],
-  ["Isabelle", "Stevens"],
-  ["James", "Bates"],
-  ["Jeffrey", "Najmi"],
-  ["Michael", "Stripling"],
-  ["Mike", "Walker"],
-  ["Prachi", "Gaur"],
-  ["Rahul", "Krishnan"],
-  ["Ronit", "Gopalani"],
-  ["Ryan", "McCarthy"],
-  ["Ryan", "Havel"],
-  ["Shad", "Khan"],
-  ["Shlok", "Khandelwal"],
-  ["Tyler", "Behr"]
-],
-
-"Gamma": [
-  ["Amanda", "Ma"],
-  ["Austin", "Hwa"],
-  ["Deep", "Patel"],
-  ["Dominique", "Rafael"],
-  ["Esther", "Zhang"],
-  ["Ethan", "Smiley"],
-  ["Ines", "Benmohamed"],
-  ["Isa", "Ahmed"],
-  ["Jenny", "Corzo"],
-  ["Katie", "Kemp"],
-  ["Kendall", "Wong"],
-  ["Nik", "Nair"],
-  ["Rafay", "Khan"],
-  ["Rida", "Habib"],
-  ["Sahir", "Mody"],
-  ["Siraj", "Ayornu"],
-  ["Timmy", "Trapaidze"]
-],
-
-"Delta": [
-  ["Amy", "Zheng"],
-  ["Andrew", "Cunniff"],
-  ["Hira", "Mahmood"],
-  ["Jasmine", "Okebugwu"],
-  ["Kati", "Ilieva"],
-  ["Katie", "Vincler"],
-  ["Lillian", "Nose"],
-  ["Matthew", "Gouzoulis"],
-  ["Ummey", "Hossain"],
-  ["Val", "Arriola-Pomasoncco"]
-],
-
-"Epsilon": [
-  ["Akriti", "Tripathi"],
-  ["John", "Gao"],
-  ["Joshua", "Choi"],
-  ["June", "Tian"],
-  ["Lauren", "LaTeef"],
-  ["Maggie", "Zhou"],
-  ["Matthew", "Leins"],
-  ["Melvyn", "Hullana"],
-  ["Nithin", "Richard"],
-  ["Priyanka", "Kishore"],
-  ["Richard", "Bui"],
-  ["Vikram", "Adipudi"],
-  ["Yomi", "Omogbehin"],
-],
-
-"Zeta": [
-  ["Andrew", "Stehman"],
-  ["Diana", "Kerr"],
-  ["Katherine", "Vo"],
-  ["Leonardo", "Wu"],
-  ["Mei", "Blatchford"],
-  ["Shreyas", "Vaderiyattil"]
-],
-
-"Eta": [
-  ["Grant", "Tamrakar"],
-  ["Kevin", "Hall"],
-  ["Steven", "Garrido"]
-]
-
-
-
-}
-}
+    ["Harleen","Kaur"],
+    ["Vid","Vashist"]
+  ],
+  
+  "Epsilon": [
+    ["Akriti", "Tripathi"],
+    ["John", "Gao"],
+    ["Joshua", "Choi"],
+    ["June", "Tian"],
+    ["Lauren", "LaTeef"],
+    ["Maggie", "Zhou"],
+    ["Matthew", "Leins"],
+    ["Melvyn", "Hullana"],
+    ["Nithin", "Richard"],
+    ["Priyanka", "Kishore"],
+    ["Richard", "Bui"],
+    ["Vikram", "Adipudi"],
+    ["Yomi", "Omogbehin"],
+    ["Rishi", "Parikh"]
+  ],
+  
+  "Zeta": [
+    ["Andrew", "Stehman"],
+    ["Diana", "Kerr"],
+    ["Katherine", "Vo"],
+    ["Leonardo", "Wu"],
+    ["Mei", "Blatchford"],
+    ["Shreyas", "Vaderiyattil"],
+    ["Sophie", "Jorgensen"],
+    ["Janaki", "Patel"]
+  ],
+  
+  "Eta": [
+    ["Grant", "Tamrakar"],
+    ["Kevin", "Hall"],
+    ["Steven", "Garrido"],
+    ["Casey", "Puentes"],
+    ["Suraj", "Narang"],
+    ["Vidit", "Makwana"]
+  ],
+  
+  "Theta": [
+    ["Patrick", "Hogan"],
+    ["Abby", "Miller"],
+    ["Zoey", "Anastasiadis"],
+    ["Lily", "Le"],
+    ["Hrishi", "Chintagumpala"]
+  ],
+  
+  "Iota": [
+    ["Joaquin", "Panambo"]
+  
+  ]
+  
+  }
+  }
+  
